df33cdc5bc7030fbbd06e1e9f90693314919b096
Sina Yeganeh <sinayega@gmail.com>
2019-03-11 17:01:21 -0700

    Added Log data struct

---
+++ Log.swift
@@ -0,0 +1,15 @@
+//
+//  Log.swift
+//  MilageCalculator
+//
+//  Created by Sina Yeganeh on 3/11/19.
+//  Copyright © 2019 Will Laxson. All rights reserved.
+//
+
+import Foundation
+
+struct Log {
+    let date: Date
+    let miles: Double
+    let gallons: Double
+}
